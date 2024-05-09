de2c914f00544fca11be8553b31b4793f09e11fa
dipankarpankaj <dipankarpankaj38@gmail.com>
2024-05-09 20:43:49 +0530

    fixes

--- resources/views/backend/milk-purchase/milk-item.blade.php
+++ resources/views/backend/milk-purchase/milk-item.blade.php
@@ -3,11 +3,11 @@
          <span class="form-control">{{$milk->name}}</span> <input name="product[name][]" class="d-none" value="{{$milk->name}}" /> 
          <input type="hidden" name="product[id][]" value="{{$milk->id}}">
     </td>
-    <td> <input type="number" class="form-control fat-content" name="product[fat_content][]" value="{{$milk->fat_content}}"/> </td>
-    <td> <input type="number" class="form-control shelf-life" name="product[shelf_life][]" value="{{$milk->shelf_life}}"/> </td>
-    <td> <input type="number" class="form-control volume" name="product[volume][]" value="{{$milk->volume}}"/> </td>
-    <td> <input type="number" class="form-control mrp" name="product[mrp][]" value="{{$milk->mrp}}"/> </td>
-    <td> <input type="number" class="form-control mop" name="product[mop][]" value="{{$milk->mop}}"/> </td>
-    <td> <input type="number" class="form-control total-amt" name="product[total_amt][]" value="{{number_format((($milk->volume)/1000) * $milk->mrp,2)}}" readonly /> </td>
+    <td> <input type="number" step="any" class="form-control fat-content" name="product[fat_content][]" value="{{$milk->fat_content}}"/> </td>
+    <td> <input type="number" step="any" class="form-control shelf-life" name="product[shelf_life][]" value="{{$milk->shelf_life}}"/> </td>
+    <td> <input type="number" step="any" class="form-control volume" name="product[volume][]" value="{{$milk->volume}}"/> </td>
+    <td> <input type="number" step="any" class="form-control mrp" name="product[mrp][]" value="{{$milk->mrp}}"/> </td>
+    <td> <input type="number" step="any" class="form-control mop" name="product[mop][]" value="{{$milk->mop}}"/> </td>
+    <td> <input type="number" step="any" class="form-control total-amt" name="product[total_amt][]" value="{{number_format((($milk->volume)/1000) * $milk->mrp,2)}}" readonly /> </td>
     <td> <button type="button" class="btn btn-outline btn-danger" onclick="removeItem(this)"><i class="bi bi-trash"></i></button></td>
 </tr>
